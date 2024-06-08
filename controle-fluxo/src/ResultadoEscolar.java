ae20f7bdff75a7c0920e328777fdbe904265d3ee
souvictorcassio <victor.cassio123@gmail.com>
2024-06-08 14:50:51 -0300

    controle de fluxo

---
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -0,0 +1,19 @@
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+
+       int nota = 7;
+       
+    //    if(nota >= 7)
+    //         System.out.println("Aprovado");
+    //     else if (nota >= 5 && nota <7)
+    //         System.out.println("Recuperação");
+    //     else
+    //         System.out.println("Reprovado");
+
+    // String resultado = nota >=7 ? "Aprovado" : "Reprovado";
+	// 	System.out.println(resultado);
+
+        String resultado = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado";
+		System.out.println(resultado);
+    }
+}
